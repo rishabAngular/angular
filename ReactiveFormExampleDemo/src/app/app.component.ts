5508a182edd22b5462fff865eeab4dfdf6c8ebb4
rishabAngular <rishabchatterjee9@gmail.com>
2023-05-22 01:10:31 +0530

    initial

---
+++ ReactiveFormExampleDemo/src/app/app.component.ts
@@ -0,0 +1,42 @@
+import { Component } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  isSubmit: boolean = false;
+  public myForm: FormGroup = new FormGroup({
+    name: new FormControl('',[Validators.required, Validators.minLength(5),Validators.maxLength(10),Validators.pattern('^[a-zA-Z ]*$')]),
+    email: new FormControl('',[Validators.required,Validators.pattern("^[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,4}$")]),
+    phone: new FormControl('',[Validators.required,Validators.minLength(18),Validators.maxLength(120), Validators.pattern(/^-?(0|[1-9]\d*)?$/)]),
+    acceptTerms: new FormControl('',[Validators.requiredTrue]),
+  });
+  constructor() { }
+
+  onSend() {
+    this.isSubmit = true;
+    console.log(this.myForm);
+  }
+  reset() {
+    this.isSubmit = false;
+    this.myForm.reset();
+  }
+
+  get name() {
+    return this.myForm.get('name')!;
+  }
+
+  get phone() {
+    return this.myForm.get('phone')!;
+  }
+
+  get email() {
+    return this.myForm.get('email')!;
+  }
+  get acceptTerms() {
+    return this.myForm.get('acceptTerms')!;
+  }
+}
